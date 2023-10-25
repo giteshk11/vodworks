0b5808404d065ed576324f7005091b3e67ac1c7a
snawaz_vodworks <snawaz@vodworks.com>
2023-10-25 13:52:01 +0500

    updated error layout

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="fixed z-50 top-0 h-screen w-screen flex justify-center items-center">
-      <div class="text-center flex-col items-center justify-center bgColor-grey w-screen h-screen flex">
-          <div v-if="error.statusCode === 404" class="mx-auto max-w-4/5">
+    <div class="error bgColor-grey">
+      <div class="xy-center">
+          <div v-if="error.statusCode === 404" class="image">
             <img class="image404" src="~/assets/img/404.svg" alt="404 illustration" />
             <h1 class="my-8">Oops!!! Page Not Found</h1>
             <NuxtLink to="/" class="btn-primary btn-md inline-block ">Back to Home</NuxtLink>
           </div>
-          <div v-else class="mx-auto max-w-4/5">
+          <div v-else class="mx-auto">
             <h1 class="font-arial-black text-2xl lg:text-3xl">
               An error occurred - {{ error.statusCode }}
             </h1>
