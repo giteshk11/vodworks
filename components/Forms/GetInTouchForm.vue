14e43f3010580389520c08260face75d0690ffda
snawaz_vodworks <snawaz@vodworks.com>
2024-02-05 11:47:01 +0500

    updated Contact Us form and blog subscription form

--- components/Forms/GetInTouchForm.vue
+++ components/Forms/GetInTouchForm.vue
@@ -1,112 +1,24 @@
 <template>
-    <!-- Note: action="thank-you.html"-->
-
-    <!-- 
-    <form autocomplete="off" name="Contact_Form" method="POST" data-netlify="true" data-netlify-honeypot="bot-field"
-        class="w-full mt-8 lg:mt-12" @submit.prevent="handleSubmit">
-
-        <input type="hidden" name="form-name" value="Contact_Form" />
-
-        <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 md:gap-8">
-            <div class="w-full">
-                <label for="fname" class="block color-secondary-black text-xsmall mb-2">
-                    Your first name<sup class="text-red-500">*</sup>
-                </label>
-                <input id="fname" v-model="formData.first_name" name="first_name" type="text"
-                    class="appearance-none block w-full focus:outline-none focus:bg-white form-control" required />
-            </div>
-            <div class="w-full">
-                <label for="lname" class="block color-secondary-black text-xsmall mb-2">
-                    Your last name<sup class="text-red-500">*</sup>
-                </label>
-                <input id="lname" v-model="formData.last_name" name="last_name" type="text"
-                    class="appearance-none block w-full focus:outline-none focus:bg-white form-control" required />
-            </div>
-            <div class="w-full">
-                <label for="email" class="block color-secondary-black text-xsmall mb-2">
-                    Your email<sup class="text-red-500">*</sup>
-                </label>
-                <input id="email" v-model="formData.email_address" name="email-address" type="email"
-                    class="appearance-none block w-full focus:outline-none focus:bg-white form-control" required />
-            </div>
-            <div class="w-full">
-                <label for="ph" class="block color-secondary-black text-xsmall mb-2">
-                    Your phone number<sup class="text-red-500">*</sup>
-                </label>
-                <input id="ph" v-model="formData.phone_number" name="phone_number" type="number"
-                    class="appearance-none block w-full focus:outline-none focus:bg-white form-control" required />
-            </div>
-        </div>
-
-        <div class="w-full my-4 md:my-8">
-            <div>
-                <label for="budgetID" class="block color-secondary-black text-xsmall mb-2">
-                    What is Your Budget?
-                </label>
-                <div class="relative">
-                    <select id="budgetID" v-model="formData.budget" name="budget"
-                        class="appearance-none block w-full focus:outline-none focus:bg-white form-control font-bold">
-                        <option disabled selected value="">***Select Your Budget*** </option>
-                        <option value="$50-100K">$50-100K</option>
-                        <option value="$100-200K">$100-200K</option>
-                        <option value="$200K+">$200K+</option>
-
-                    </select>
-                    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-4">
-                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                        </svg>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-        <div class="w-full">
-            <label for="details" class="block color-secondary-black text-xsmall mb-2">
-                Message<sup class="text-red-500">*</sup>
-            </label>
-            <textarea id="details" v-model="formData.message" name="message" rows="8" class="form-control w-full"
-                placeholder="Tell us a little more about your project. Are you starting a new software project or do you need help with an existing one? Do you already have a list of requirements and features or are you starting from scratch?"
-                required></textarea>
-        </div>
-
-        <div v-if="isUrlContact">
-            <div class="custom-checkbox mt-6">
-                <input id="RequestNDA" v-model="formData.isRequestNDAchecked" type="checkbox" name="RequestNDA"
-                    value="Yes Request NDA" />
-                <span class="checkbox"></span>
-                <label for="RequestNDA">Request NDA</label>
-            </div>
-
-        </div>
-
-        <div>
-            <button type="submit" class="btn-primary btn-lg inline-block mt-10 lg:mt-16 w-80">
-                Submit
-            </button>
-        </div>
-
-
-        <div v-if="isSuccessPopupOpened" class="overlay"></div>
-        <div v-if="isSuccessPopupOpened" class="success text-center">
-            <h2>Thank you for contacting us!</h2>
-
-            <div class="block  mt-8 lg:mt-12 mb-8">
-                <buttton class="btn-primary btn-md cursor-pointer" @click="closeMe">
-                    Submit Another Form
-                </buttton>
-            </div>
-            <div class="block">
-                <buttton class="btn-text inline-block cursor-pointer" type="submit" @click="closeMe">Close</buttton>
-            </div>
-        </div>
-
-
-    </form>
-    -->
-
     <div>
-        <div id="hubspotForm" class="w-full mt-8 lg:mt-12"></div>
+        <div>
+            <div id="hubspotForm" class="w-full mt-8 lg:mt-12">
+            </div>
+        </div>
+
+        <div class="overlay cursor-pointer" @click="closeSuccess"></div>
+        <div class="success">
+            <div class="text-center">
+                <h2>Thank You!</h2>
+                <p class="mt-4">Thank you for contacting us, we will get back to you as soon as possible.</p>
+                <div class="block mt-8">
+                    <button class="btn-primary btn-md cursor-pointer" @click="closeSuccess">
+                        Close
+                    </button>
+                </div>
+               
+            </div>
+
+        </div>
     </div>
 </template>
   
@@ -116,86 +28,60 @@ export default {
 
     name: 'GetInTouchForm',
 
-    data() {
-        return {
-            isSuccessPopupOpened: false,
-
-            formData: {
-                first_name: "",
-                last_name: "",
-                email_address: "",
-                phone_number: "",
-                budget: "",
-                message: "",
-                isRequestNDAchecked: false,
-            }
-        }
-    },
-
-    computed: {
-        isUrlContact() {
-            return this.$route.path === '/contact'
-        },
-    },
-
-    watch: {
-        isUrlContact() {
-            return this.$route.path === '/contact'
-        },
-    },
-
     mounted() {
         const script = document.createElement("script");
         script.src = "https://js.hsforms.net/forms/v2.js";
         document.body.appendChild(script);
+
         script.addEventListener("load", () => {
             if (window.hbspt) {
                 window.hbspt.forms.create({
                     region: "na1",
                     portalId: "1873794",
                     formId: "e7440546-49d4-48e1-9252-ea8fd906931b",
-                    target: "#hubspotForm"
+                    target: "#hubspotForm",
+
+                    // onFormReady($form) {
+                    //     // Stuff to do on after form is loaded             
+                    // },
+                    onFormSubmit($form, data) {
+                        // Stuff do do before form is submitted 
+                        /* eslint-disable no-console */
+                        console.log("$form: ", $form)
+                        console.log("data: ", data)
+                        // this.isSuccessPopupOpened = !this.isSuccessPopupOpened
+                        // eslint-disable-next-line no-undef
+                        // $('.success').show();
+                        // document.querySelector(".success").style.display = "block";
+                        const el = document.querySelector(".success");
+                        const el2 = document.querySelector(".overlay");
+                        el.classList.add("show");
+                        el2.classList.add("show");
+                    }
                 })
             }
-        })
+        });
+
+        window.addEventListener('message', event => {
+            if (event.data.type === 'hsFormCallback' && event.data.eventName === 'onFormSubmitted') {
+                window.hbspt.forms.create({
+                    region: "na1",
+                    portalId: "1873794",
+                    formId: event.data.id,
+                    target: "#hubspotForm" // Add the element class where you need to create the form again
+                });
+            }
+        });
     },
 
     methods: {
-
-        handleSubmit(event) {
-            event.preventDefault();
-            const myForm = event.target;
-            const contactformData = new FormData(myForm);
-
-            fetch("/", {
-                method: "POST",
-                headers: { "Content-Type": "application/x-www-form-urlencoded" },
-                body: new URLSearchParams(contactformData).toString(),
-            })
-                .then(
-                    this.isSuccessPopupOpened = !this.isSuccessPopupOpened,
-                    // event.target.reset()
-
-                    this.formData.first_name = '',
-                    this.formData.last_name = '',
-                    this.formData.email_address = '',
-                    this.formData.phone_number = '',
-                    this.formData.budget = '',
-                    this.formData.message = '',
-                    this.formData.isRequestNDAchecked = false,
-
-                )
-                .catch((error) => alert(error));
-        },
-
-        closeMe() {
-            this.isSuccessPopupOpened = !this.isSuccessPopupOpened;
-        },
-
+        closeSuccess() {
+            const el = document.querySelector(".success");
+            const el2 = document.querySelector(".overlay");
+            el.classList.remove("show");
+            el2.classList.remove("show");
+        }
     },
-
-
 
 }
 </script>
-  
--- components/Sections/SubscribeToOurBlog.vue
+++ components/Sections/SubscribeToOurBlog.vue
@@ -3,61 +3,12 @@
     <section class="lg:py-32 py-14 bgColor-tertiary-black color-white">
         <div class="mx-auto container">
 
-            <div class="text-center">
-                <!--<form class="blog-subscription-form">
-                    <h2 class="heading-1">Subscribe to our blog</h2>
-                    <input id="email" name="email-address" type="email" placeholder="Your email"
-                        class="appearance-none block w-full focus:outline-none focus:bg-white form-control mt-8 lg:mt-12  mb-8 lg:mb-12"
-                        required />
-                    <button type="button" class="btn-primary btn-lg inline-block">
-                        Subscribe
-                    </button>
-                </form>
-                -->
-
-                <div id="mc_embed_shell">
-                    <!-- <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css"> -->
-
-
-                    <div id="mc_embed_signup">
-                        <h2 class="heading-1">Subscribe to our blog</h2>
-
-                        <form autocomplete="off"
-                            action="https://vodworks.us14.list-manage.com/subscribe/post?u=94e39ce3ae8526c7c6078b57d&amp;id=c1d1d4679c&amp;f_id=00e152e0f0"
-                            method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate"
-                            target="_self" novalidate="">
-                            <div id="mc_embed_signup_scroll">
-
-                                <div class="mc-field-group">
-                                    <input ref="email" v-model="email_p" type="email" name="EMAIL" class="required email"
-                                        id="mce-EMAIL" required="" value="" @keyup="validateMail">
-                                    <p v-show="!isButtonEnabled" class="text-left mt-2 mb-4" style="color:red !important;">
-                                        {{ errorMessage }}</p>
-                                </div>
-                                <div hidden="">
-                                    <input type="hidden" name="tags" value="8439029">
-                                </div>
-                                <div id="mce-responses" class="clear">
-                                    <div class="response" id="mce-error-response" style="display: none;"></div>
-                                    <div class="response" id="mce-success-response" style="display: none;"></div>
-                                </div>
-                                <div aria-hidden="true" style="position: absolute; left: -5000px;">
-                                    <input type="text" name="b_94e39ce3ae8526c7c6078b57d_c1d1d4679c" tabindex="-1" value="">
-                                </div>
-
-                                <div class="clear">
-                                    <input :disabled="!isButtonEnabled" type="submit" name="subscribe"
-                                        id="mc-embedded-subscribe" class="button btn-primary btn-lg w-80 cursor-pointer"
-                                        value="Subscribe">
-                                </div>
-                            </div>
-                        </form>
+            <div class="text-center mx-auto md:max-w-3/5">
+                <h2 class="heading-1">Subscribe to our blog</h2>
+                <div class=" mx-auto md:max-w-4/5">
+                    <div id="blogSubscriptionForm" class="w-full mt-8 lg:mt-12">
                     </div>
-
                 </div>
-
-
-
             </div>
 
         </div>
@@ -69,78 +20,24 @@
 
 export default {
     name: 'SubscribeToOurBlog',
-    props: {
-        data: {
-            type: Object,
-            default: null
-        }
-    },
-    data() {
-        return {
-            isSuccessPopupOpened: false,
-            isButtonEnabled: false,
-            email_p: '',
-            errorMessage: '',
-        };
-    },
 
-    // watch: {
-    //     email(value) {
-    //         // binding this to the data value in the email input
-    //         this.email = value;
-    //         this.validateEmail(value);
-    //     },
+    mounted() {
+        const script = document.createElement("script");
+        script.src = "https://js.hsforms.net/forms/v2.js";
+        document.body.appendChild(script);
 
-    // },
-    methods: {
-        // validateEmail(email) {
-        //     // eslint-disable-next-line no-useless-escape
-        //     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
-        //         this.errorMessage = ''
-        //     } else {
-        //         this.errorMessage = 'Invalid Email'
-        //     }
-        // },
-
-        validateMail(value) {
-            const mailformat = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
-            if (value.target.value.match(mailformat)) {
-                this.isButtonEnabled = true
-                this.errorMessage = ''
-            } else {
-                this.errorMessage = 'Enter a valid email'
-                this.isButtonEnabled = false
+        script.addEventListener("load", () => {
+            if (window.hbspt) {
+                window.hbspt.forms.create({
+                    region: "na1",
+                    portalId: "1873794",
+                    formId: "19f7d7c4-7d7f-4733-9e52-3473df7b78a5",
+                    target: "#blogSubscriptionForm",
+                })
             }
-        },
+        });
 
 
-        // formSubmit() {
-        //     if (this.errorMessage !=="") {
-        //         this.$router.push('/thanks')
-        //     }
-        // }
     },
-
-
-    // methods: {
-    //     formSubmit() {
-    //         if (this.EMAIL) {
-    //             this.$router.push('/thanks')
-    //         }
-    //     }
-    // }
-
-
-
 }
 </script>
-
-<style>
-input[disabled='disabled'] {
-    opacity: 0.6;
-    cursor: not-allowed;
-}
-</style>
-  
-  
